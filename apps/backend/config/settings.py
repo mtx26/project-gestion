d22f472a9b019812106ed03d20ef10a548072b36
mtx26 <mtx12345626@gmail.com>
2026-06-19 00:47:39 +0200

    push

--- apps/backend/config/settings.py
+++ apps/backend/config/settings.py
@@ -110,10 +110,10 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(
+        "DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    )
 }
 
 
